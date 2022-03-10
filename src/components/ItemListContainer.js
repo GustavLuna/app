ac8236e0ac19eb02be29b7f2ba021d1f3131a4f5
GustavLuna <gustav.luna@hotmail.com>
2022-03-09 21:03:21 -0300

    Update ItemListContainer.js
    actualizando itemlistcontainer

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,19 +1,85 @@
 import ItemCount from "./ItemCount"
+import { useState, useEffect } from "react"
+import ItemList from "./ItemList";
+import Item from './Item'
 
-const ItemListContainer = (props) => {
+let productosIniciales = [
+    {
+        id: 1,
+        nombre1: "Producto 1",
+        precio: 100
+    },
+    {
+        id: 2,
+        nombre2: "Producto 2",
+        precio: 200
+    },
+    {
+        id: 3,
+        nombre3: "Producto 3",
+        precio: 300
+    }
+]
 
-    const onAdd = (value)=>{
+const Main = (props) => {
+
+    const onAdd = (value) => {
         alert(`agregaste ${value} productos al carrito`)
-      } 
+    }
+
+    const [loading, setLoading] = useState(true)
+    const [productos, setProductos] = useState([])
+
+    useEffect(() => {
+
+        const promesa = new Promise((res,rej)=>{
+            setTimeout(()=>{
+                res(productosIniciales)
+            },3000)
+        })
+
+        promesa
+        .then((respuestaDeLaApi)=>{
+            setProductos(productosIniciales)
+        })
+        .catch((errorDeLaApi)=>{
+            console.log(errorDeLaApi)
+           
+        })
+        .finally(()=>{
+            setLoading(false)
+        })
+    })
 
     return (
-            <itemlistcontainer className="container">
-                <h1>Bienvenidos  {props.greeting}!</h1>
-            <ItemCount initial={1} stock={5} onAdd={onAdd}/>
-            </itemlistcontainer>
-    )
+        <main className="container">
+            <h1>Bienvenidos  {props.greeting}!</h1>
+            <ItemCount initial={1} stock={5} onAdd={onAdd} />
+            <p>{loading ? "Cargando..." : "Ya tenes los productos"}</p>
+            <ItemList>
+            <Item/>
+                </ItemList>
+        </main>
+    );
 }
 
-export default ItemListContainer
+export default Main;
+
+
+// const ItemListContainer = (props) => {
+
+//     const onAdd = (value)=>{
+//         alert(`agregaste ${value} productos al carrito`)
+//       }
+
+//     return (
+//             <itemlistcontainer className="container">
+//                 <h1>Bienvenidos  {props.greeting}!</h1>
+//             <ItemCount initial={1} stock={5} onAdd={onAdd}/>
+//             </itemlistcontainer>
+//     )
+// }
+
+// export default ItemListContainer
 
 
